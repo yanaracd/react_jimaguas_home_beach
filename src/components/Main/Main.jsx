e5713cb76f0ac1b25ce43e568ec3b5cf5136c06c
Yanara Caballero Delgado <yanaracaballero97@gmail.com>
2023-10-24 16:56:23 +0200

    main v.0.1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,7 @@ import { Slider } from "./Slider"
 import { Accordion } from "../Accordion/Accordion"
 import { Location } from "./Location"
 import { Host } from "./Host"
+import { Service } from "./Service"
 
 export const ActiveContext = createContext()
 
@@ -22,6 +23,7 @@ export const Main = () => {
                 <Room />
                 <Lightbox />
                 <Accordion />
+                <Service />
                 <Location />
                 <Host />
             </main>  
---
+++ src/components/Main/Service.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import './Service.css'
+
+export const Service = () => {
+
+    const [ servicios, setServicios ] = useState([])
+
+    useEffect(() => {
+
+        let { VITE_API_URL } = import.meta.env || 'http://localhost:3000'
+
+        let options = {
+            method: 'get'
+        }
+
+        fetch(`${VITE_API_URL}/servicios`, options)
+            .then(res => res.json())
+            .then(data => setServicios(data))
+
+    }, [])
+
+    return (
+        <section className="Main-div Service" id="Servicios">
+            <h2 className="Service-h2">Servicios</h2>
+            <div className="Service-wrapper">
+                {
+                    servicios.map( servicio =>
+                        <ArticleService 
+                            key={servicio._id}
+                            { ...servicio } />
+                    )
+                }
+            </div>
+        </section>
+    )
+}
+
+const ArticleService = ( props ) => {
+
+    const { className , img , button , h3 } = props
+
+    return (
+        <article className={`Service-article ${className}`}>
+            <div className="Service-div">
+                <img loading="lazy" src={img.src} alt={img.alt} className="Service-img" />
+                <button className="Service-button" title="Saber mas">{button}</button>
+            </div>
+            <h3 className="Service-h3">{h3}</h3>
+        </article>
+    )
+}

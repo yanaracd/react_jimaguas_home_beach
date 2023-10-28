275af13d2ac27e716a36e111e9f9f286e19d14e0
Yanara Caballero Delgado <yanaracaballero97@gmail.com>
2023-10-28 13:05:22 +0200

    reservas v.0.1

--- src/components/Booking/Manager.jsx
+++ src/components/Booking/Manager.jsx
@@ -1,35 +1,47 @@
-import { useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
 import './Manager.css'
+
+const ManagerContext = createContext()
 
 export const Manager = () => {
 
+    const [reservas, setReservas] = useState([])
+    const [update, setUpdate] = useState(false)
+    const idInput = useRef()
+    const entradaInput = useRef()
+    const salidaInput = useRef()
+    const cantidadInput = useRef()
+    const habInput = useRef()
+
     return (
-        <div className="Manager">
-            <div className="Manager-wrapper">
-                <h2 className="Manager-h2">Reservas</h2>
-                <TableManager />
+        <ManagerContext.Provider value={{ reservas, setReservas, update, setUpdate, idInput, entradaInput, salidaInput, cantidadInput, habInput }}>
+            <div className="Manager">
+                <div className="Manager-wrapper">
+                    <h2 className="Manager-h2">Reservas</h2>
+                    <TableManager />
+                </div>
             </div>
-        </div>
+        </ManagerContext.Provider>
     )
 }
 
 const TableManager = () => {
 
-    const [ reservas , setReservas ] = useState([])
+    const { reservas, setReservas } = useContext(ManagerContext)
 
-    useEffect(()=>{
+    useEffect(() => {
 
         let { VITE_API_URL } = import.meta.env || 'http://localhost:3000'
-        
+
         let options = {
-            method  : 'get'
+            method: 'get'
         }
 
-        fetch( `${VITE_API_URL}/reservas` , options )
-        .then( res => res.json() )
-        .then( data => setReservas(data) )
+        fetch(`${VITE_API_URL}/reservas`, options)
+            .then(res => res.json())
+            .then(data => setReservas(data))
 
-    },[])
+    }, [])
 
     return (
         <table className="table">
@@ -37,12 +49,13 @@ const TableManager = () => {
                 <TheadTable />
             </thead>
             <tbody>
+                <TableUpdate />
                 {
-                    reservas.map( ( reserva , i ) =>
+                    reservas.map((reserva, i) =>
                         <TbodyTable
                             key={reserva._id}
-                            { ...reserva }
-                            i = {i+1} />
+                            {...reserva}
+                            i={i + 1} />
                     )
                 }
             </tbody>
@@ -65,9 +78,37 @@ const TheadTable = () => {
     )
 }
 
-const TbodyTable = ( props ) => {
+const TbodyTable = (props) => {
 
-    const { i , entrada , salida , cantidad , hab } = props 
+    const { i, _id, entrada, salida, cantidad, hab } = props
+    const { reservas, setReservas, setUpdate, idInput, entradaInput, salidaInput, cantidadInput, habInput } = useContext(ManagerContext)
+
+    const deleteBooking = (id) => {
+
+        let { VITE_API_URL } = import.meta.env || 'http://localhost:3000'
+
+        let options = {
+            method: 'delete'
+        }
+
+        fetch(`${VITE_API_URL}/reservas/${id}`, options)
+            .then(res => res.json())
+            .then(data => setReservas(data))
+
+    }
+
+    const updateBooking = (idReserva) => {
+        setUpdate(true)
+
+        let buscar = reservas.find(reserva => reserva._id === idReserva)
+        let { entrada, salida, cantidad, hab } = buscar
+
+        idInput.current.value = idReserva
+        entradaInput.current.value = entrada
+        salidaInput.current.value = salida
+        cantidadInput.current.value = cantidad
+        habInput.current.value = hab
+    }
 
     return (
         <tr>
@@ -78,8 +119,72 @@ const TbodyTable = ( props ) => {
             {
                 hab === '3' ? <td>Alojamiento completo</td> : <td>{`Habitación ${hab}`}</td>
             }
-            <td><button type="button" className="btn btn-primary">Modificar</button></td>
-            <td><button type="button" className="btn btn-danger">Eliminar</button></td>
+            <td><button onPointerDown={() => updateBooking(_id)} type="button" className="btn btn-primary">Modificar</button></td>
+            <td><button onPointerDown={() => deleteBooking(_id)} type="button" className="btn btn-danger">Eliminar</button></td>
+        </tr>
+    )
+}
+
+const TableUpdate = () => {
+
+    const { setReservas, update, setUpdate, idInput, entradaInput, salidaInput, cantidadInput, habInput } = useContext(ManagerContext)
+
+    const updateBooking = (e) => {
+        e.preventDefault()
+
+        let { value: _id } = idInput.current
+        let { value: entrada } = entradaInput.current
+        let { value: salida } = salidaInput.current
+        let { value: cantidad } = cantidadInput.current
+        let { value: hab } = habInput.current
+
+        let nuevo = { _id, entrada, salida, cantidad, hab }
+        
+        let { VITE_API_URL } = import.meta.env || 'http://localhost:3000'
+        
+        let options = {
+            method  : 'put',
+            body    : JSON.stringify(nuevo),
+            headers : {
+                "Content-type" : "application/json"
+            }
+        }
+
+        fetch(`${VITE_API_URL}/reservas`, options)
+            .then(res => res.json())
+            .then(data => setReservas(data))
+
+        setUpdate(false)
+    }
+
+    return (
+        <tr className={`Manager-update ${update ? 'isActive' : ''}`}>
+            <th scope="row">
+                <input type="hidden" ref={idInput} />
+            </th>
+            <td>
+                <input id="llegada" type="date" min="2023-01-01" required ref={entradaInput} />
+            </td>
+            <td>
+                <input id="salida" type="date" min="2023-01-01" required ref={salidaInput} />
+            </td>
+            <td>
+                <select name="select" ref={cantidadInput}>
+                    <option value="1">1 Huésped</option>
+                    <option value="2">2 Huéspedes</option>
+                    <option value="3">3 Huéspedes</option>
+                    <option value="4">4 Huéspedes</option>
+                </select>
+            </td>
+            <td>
+                <select name="select" ref={habInput}>
+                    <option value="1">Habitación 1</option>
+                    <option value="2">Habitación 2</option>
+                    <option value="3">Alojamiento completo</option>
+                </select>
+            </td>
+            <td><button onPointerDown={updateBooking} type="button" className="btn btn-primary">Modificar</button></td>
+            <td></td>
         </tr>
     )
 }
---
+++ src/components/Main/FormBooking.jsx
@@ -0,0 +1,72 @@
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export const Form = () => {
+
+    const [ message , setMessage ] = useState()
+    const navigate      = useNavigate()
+    const entradaInput  = useRef()
+    const salidaInput   = useRef()
+    const cantidadInput = useRef()
+    const habInput      = useRef()
+
+    const formSubmit = ( e ) => {
+        e.preventDefault()
+
+        const { value : entrada  } = entradaInput.current
+        const { value : salida   } = salidaInput.current
+        const { value : cantidad } = cantidadInput.current
+        const { value : hab      } = habInput.current
+
+        const reserva = { entrada, salida, cantidad , hab }
+
+        let { VITE_API_URL } = import.meta.env || 'http://localhost:3000'
+        
+        let options = {
+            method  : 'post',
+            body    : JSON.stringify(reserva),
+            headers : {
+                "Content-type" : "application/json"
+            }
+        }
+
+        fetch( `${VITE_API_URL}/reservas` , options )
+        .then( res => res.json() )
+        .then( data => {
+            setMessage(data)
+            navigate('/home/booking')            
+        })
+
+        entradaInput.current.value  = ''
+        salidaInput.current.value   = ''
+    }
+
+    return(
+        <form onSubmit={formSubmit} className="Home-form" action="/send/" method="post">
+            <h3 className="Home-h3">Selecione la fecha de su reserva</h3>
+            <ul className="Home-reserva">
+                <li className="Home-fecha">
+                    <label className="Home-label">Llegada</label>
+                    <input className="Home-input" id="llegada" type="date" min="2023-01-01" required ref={entradaInput} />
+                </li>
+                <li className="Home-fecha">
+                    <label className="Home-label">Salida</label>
+                    <input className="Home-input" id="salida" type="date" min="2023-01-01" required ref={salidaInput} />
+                </li>
+            </ul>
+            <h3 className="Home-h3">Selecione la cantidad de huéspedes</h3>
+            <select className="Home-select" name="select" ref={cantidadInput}>
+                <option className="Home-option" value="1">1 Huésped</option>
+                <option className="Home-option" value="2">2 Huéspedes</option>
+                <option className="Home-option" value="3">3 Huéspedes</option>
+                <option className="Home-option" value="4">4 Huéspedes</option>
+            </select>
+            <select className="Home-select" name="select" ref={habInput}>
+                <option className="Home-option" value="1">Habitación 1</option>
+                <option className="Home-option" value="2">Habitación 2</option>
+                <option className="Home-option" value="3">Alojamiento completo</option>
+            </select>
+            <input type="submit" className="Home-send" value="Reservar" />
+        </form>
+    )
+}

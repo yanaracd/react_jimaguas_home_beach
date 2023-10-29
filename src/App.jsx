fda82ad311ff34f639fd6d7edeee411f04b07769
Yanara Caballero Delgado <yanaracaballero97@gmail.com>
2023-10-29 22:32:29 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { LoginPage } from './pages/LoginPage'
 import { Home } from './pages/Home'
@@ -20,6 +20,7 @@ function App() {
           <Route path='/:param'       element={<LoginPage />} />
           <Route path='/home'         element={<Home      />} />
           <Route path='/home/booking' element={<Booking   />} />
+          <Route path='*'             element={<Navigate to='/' />} />
         </Routes> 
 
       </AppContext.Provider> 

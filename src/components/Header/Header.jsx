0f7f8d7536092efd777dc8aabb8fce063d709f15
Yanara Caballero Delgado <yanaracaballero97@gmail.com>
2023-10-29 11:35:09 +0100

    header v.0.3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import { createContext, useContext, useState } from 'react'
 import './Header.css'
 import { useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
 import { AppContext } from '../../App'
 
 const MenuContext = createContext()
@@ -31,7 +30,8 @@ const HeaderLogo = () => {
 
     return (
         <h1 className="Header-h1">
-            <a className="Header-a" href={`${VITE_WEB_URL}/home#Inicio`} title="Inicio">
+            {/* <a className="Header-a" href={`${VITE_WEB_URL}/home#Inicio`} title="Inicio"> */}
+            <a className="Header-a" href={`http://localhost:5173/home#Inicio`} title="Inicio">
                 <img className="Header-img" src="assets/logo_web.png" alt="Logo Jimaguas Home Beach" />
             </a>
         </h1>
@@ -68,7 +68,6 @@ const HeaderNav = () => {
                     )
                 }
             </ul>
-            <HeaderUser />
         </nav>
     )
 }
@@ -80,22 +79,24 @@ const HeaderNavLi = (props) => {
 
     return (
         <li className="Header-li">
-            <a className="Header-link" href={`${VITE_WEB_URL}/home${href}`} title={title}>{nombre}</a>
+            {/* <a className="Header-link" href={`${VITE_WEB_URL}/home${href}`} title={title}>{nombre}</a> */}
+            <a className="Header-link" href={`http://localhost:5173/home${href}`} title={title}>{nombre}</a>
         </li>
     )
 }
 
-const HeaderUser = () => {
-    return (
-        <div className="Header-user">
-            <svg className='Header-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-            </svg>
-            <h2 className="Header-h2">UserName</h2>
-        </div>
-    )
-}
+//Funcionalidad para próximas versiones de la web
+// const HeaderUser = () => {
+//     return (
+//         <div className="Header-user">
+//             <svg className='Header-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+//                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+//                 <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+//             </svg>
+//             <h2 className="Header-h2">UserName</h2>
+//         </div>
+//     )
+// }
 
 const HeaderButton = () => {
 
